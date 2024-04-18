bb55cff8423d4450c100b4a357fcc5afae79e7bc
sonnka <sonyakazanceva1331@gmail.com>
2024-04-18 22:35:12 +0300

    Set up configuration

---
+++ src/main/java/com/logihub/model/entity/User.java
@@ -0,0 +1,44 @@
+package com.logihub.model.entity;
+
+import com.logihub.model.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@SuperBuilder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "avatar")
+    private String avatar;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+}
