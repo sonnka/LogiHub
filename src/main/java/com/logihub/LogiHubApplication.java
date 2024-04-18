c40b381b617f8562716a4b24cbb8104dc6b24b85
sonnka <sonyakazanceva1331@gmail.com>
2024-04-18 20:54:48 +0300

    Initial commit

---
+++ src/main/java/com/logihub/LogiHubApplication.java
@@ -0,0 +1,13 @@
+package com.logihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogiHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogiHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/logihub/LogiHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logihub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogiHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
